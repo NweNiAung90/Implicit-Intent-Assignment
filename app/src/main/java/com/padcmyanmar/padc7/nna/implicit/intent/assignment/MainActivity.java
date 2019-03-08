14c79e543641c5c0ad249079b696d78225cb42f0
NweNiAung <nweniaung90@gmail.com>
2019-03-08 11:27:12 +0630

    add UI

--- app/src/main/java/com/padcmyanmar/padc7/nna/implicit/intent/assignment/MainActivity.java
+++ app/src/main/java/com/padcmyanmar/padc7/nna/implicit/intent/assignment/MainActivity.java
@@ -2,6 +2,7 @@ package com.padcmyanmar.padc7.nna.implicit.intent.assignment;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
@@ -12,9 +13,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import java.util.Calendar;
@@ -22,6 +26,12 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_SELECT_CONTACT = 1;
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+    private long startMillis = 0;
+    private long endMillis = 0;
+    private VideoView videoView;
+    private boolean videoFlag = false;
+    private boolean contactFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,26 +39,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        long startMillis = 0;
-        Calendar beginTime = Calendar.getInstance();
-        beginTime.set(2019, 2, 23, 6, 00);
-        startMillis = beginTime.getTimeInMillis();
 
         /* Create A Timer */
-        createAlarm("Go to sleep",1,0);
+        Button alarm = findViewById(R.id.btn_alarm);
+        alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAlarm("Go to sleep", 1, 0);
+            }
+        });
 
         /* Create A Calender Event */
-       addEvent("Staff Party", "IBC",startMillis,21);
+        Button calendarEvent = findViewById(R.id.btn_calendar_event);
+        calendarEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar beginTime = Calendar.getInstance();
+                beginTime.set(2019, 2, 23, 18, 00);
+                startMillis = beginTime.getTimeInMillis();
+                Calendar endTime = Calendar.getInstance();
+                endTime.set(2019, 2, 23, 21, 00);
+                endMillis = beginTime.getTimeInMillis();
+                addEvent("Staff Party", "IBC", startMillis, endMillis);
+            }
+        });
 
         /* Capture a video with implicit intent and show it back in App with auto-play (need to use VideoView). */
-        capturePhoto();
+        Button video = findViewById(R.id.btn_video);
+
+        video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                videoView = findViewById(R.id.vv_video);
+                captureVideo(videoView);
+            }
+        });
 
         /* Select a contact and display its info back in the App.*/
-        selectContact();
-        //viewContact();
+        Button contact = findViewById(R.id.btn_contact);
 
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectContact();
+            }
+        });
         /* Perform a web search with query string from the App.*/
-        searchWeb("adroid");
+        Button btnSearch = findViewById(R.id.btn_search);
+        final EditText etSearch = findViewById(R.id.et_search);
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String search = etSearch.getText().toString();
+                searchWeb(search);
+
+            }
+        });
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,28 +106,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void createAlarm(String message, int hour, int minutes) {
@@ -103,18 +130,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void capturePhoto() {
-        Intent intent = new Intent(MediaStore.INTENT_ACTION_VIDEO_CAMERA);
+    public void captureVideo(VideoView v) {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
         if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            startActivityForResult(intent, REQUEST_VIDEO_CAPTURE);
         }
-    }
-
-    public void onButtonClick(View v) {
-        VideoView videoview = (VideoView) findViewById(R.id.videoview);
-        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/");
-        videoview.setVideoURI(uri);
-        videoview.start();
+        videoFlag = true;
     }
 
     public void selectContact() {
@@ -123,19 +144,48 @@ public class MainActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(intent, REQUEST_SELECT_CONTACT);
         }
+        contactFlag = true;
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_SELECT_CONTACT && resultCode == RESULT_OK) {
-            Uri contactUri = data.getData();
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (videoFlag == true) {
+            if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+                Uri videoUri = intent.getData();
+                videoView.setVideoURI(videoUri);
+                videoView.start();
+            }
         }
-    }
 
-    public void viewContact(Uri contactUri) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, contactUri);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+        if (contactFlag == true) {
+            if (requestCode == REQUEST_SELECT_CONTACT && resultCode == RESULT_OK) {
+                Uri contactUri = intent.getData();
+                Intent data = new Intent(Intent.ACTION_VIEW, contactUri);
+                if (data.resolveActivity(getPackageManager()) != null) {
+                    startActivity(data);
+                    TextView tvName = findViewById(R.id.tv_name);
+                    TextView tvPhone = findViewById(R.id.tv_phone);
+
+                    Cursor cursor = getContentResolver().query(contactUri,
+                            null, null, null, null);
+
+                    try {
+                        if (cursor.moveToFirst()) {
+                            String name = cursor.getString(0);
+                            if (name != null) {
+                                tvName.setText(name);
+                            }
+                            String phone = cursor.getString(1);
+                            if (phone != null) {
+                                tvPhone.setText(phone);
+                            }
+                        }
+                    } finally {
+                        cursor.close();
+                    }
+
+                }
+            }
         }
     }
 
@@ -145,5 +195,27 @@ public class MainActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
